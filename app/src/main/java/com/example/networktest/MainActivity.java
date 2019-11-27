0ecc392c5a0471a9a9a19518b9551c4862a0d71c
youpeisheng <1426890389@qq.com>
2019-11-27 12:26:09 +0800

    新增OkHttp 连接网络 和Pull 解析获取内容

--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -3,21 +3,31 @@ package com.example.networktest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView responseText;
+    private String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +39,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         if(view.getId()==R.id.send_request){
-            sendRequesWithHttpURLConnection();
+//           sendRequesWithHttpURLConnection();
+            sendRequestWithokHttp();
+        }
+    }
+    private void sendRequestWithokHttp(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+            try {
+                OkHttpClient client=new OkHttpClient();
+                Request request=new Request.Builder()
+                        .url("http://192.168.31.225/get_data.xml") //url("http://10.0.2.2/get_data.xml") http://127.0.0.1/get_data.xml
+                        .build();
+                Response response=client.newCall(request).execute();
+                String responseData=response.body().string();
+                parseXMLWithPull(responseData);
+                showResponse(responseData);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            }
+        }).start();
+    }
+    private void parseXMLWithPull(String xmlData){
+        try {
+            XmlPullParserFactory factory=XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser=factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType=xmlPullParser.getEventType();
+            String id="";
+            String name="";
+            String version="";
+            while (eventType!=XmlPullParser.END_DOCUMENT){
+                String nodeName=xmlPullParser.getName();
+                switch (eventType){
+                    //开始解析某个节点
+                    case XmlPullParser.START_TAG:{
+                        if("id".equals(nodeName)){
+                            id=xmlPullParser.nextText();
+                        }else if("name".equals(nodeName)){
+                            name=xmlPullParser.nextText();
+                        }else if("version".equals(nodeName)){
+                            version=xmlPullParser.nextText();
+                        }
+                        break;
+                    }
+                    case XmlPullParser.END_TAG:{
+                        if("app".equals(nodeName)){
+                            Log.d(TAG,"id is "+id);
+                            Log.d(TAG,"name is "+name);
+                            Log.d(TAG,"version is "+version);
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                eventType=xmlPullParser.next();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
     private void sendRequesWithHttpURLConnection(){
@@ -40,7 +110,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 HttpURLConnection connection=null;
                 BufferedReader reader=null;
                 try {
-                    URL url=new URL("https://www.baidu.com");
+                    URL url=new URL("http://192.168.31.225/get_data.xml");
                     connection=(HttpURLConnection)url.openConnection();
                     connection.setRequestMethod("GET");
                     connection.setConnectTimeout(8000);
